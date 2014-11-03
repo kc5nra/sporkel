28203162433c8ab8c39ac8cc5305ce6c29238659
John R. Bradley <jrbradley@gmail.com>
2014-11-03 17:48:27 -0600

    Initial commit; Add libsodium dependency

---
+++ ext/libsodium_override/runtime.h
@@ -0,0 +1,10 @@
+#ifndef sodium_runtime_override_H
+#define sodium_runtime_override_H
+
+#ifdef _MSC_VER
+# include <intrin.h>
+#endif
+
+#include "../libsodium/src/libsodium/include/sodium/runtime.h"
+
+#endif
