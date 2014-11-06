8313f602cc9b8f8a1393aa9ce0fe71797df32020
Palana <palana@stunned.de>
2014-11-06 04:55:33 +0100

    Fix after_hash (should be the same as before hash for identical files)

--- deltagen/deltagen.cpp
+++ deltagen/deltagen.cpp
@@ -160,7 +160,7 @@ int create(char *before_tree, char *after_tree, char *patch_file)
 
 	delta_op_toc toc;
 	toc.before_hash = get_tree_hash(before_tree_state);
-	toc.after_hash = get_tree_hash(after_tree_state);
+	toc.after_hash = get_tree_hash(after_tree_state_unmod);
 	
 	printf("before tree: '%s'\n", before_path.generic_string().c_str());
 	printf("    hash: '%s'\n", toc.before_hash.c_str());
