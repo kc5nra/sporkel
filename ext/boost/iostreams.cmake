477f7f2aa5c2fdf25b1facb84e8219ebde65711c
John R. Bradley <jrb@turrettech.com>
2014-11-08 21:04:50 -0600

    Add lzma iostreams filter

--- ext/boost/iostreams.cmake
+++ ext/boost/iostreams.cmake
@@ -1,14 +1,21 @@
 project(boost-iostreams)
 
+if(LIBLZMA_CONFIG)
+	add_definitions(
+		-DLZMA_API_STATIC)
+endif()
+
+include_directories(
+	${LIBLZMA_INCLUDE_DIRS})
+
 set(boost-iostreams_SOURCES
-	iostreams/src/bzip2.cpp)
+	iostreams/src/bzip2.cpp
+	iostreams_lzma/src/lzma.cpp)
 
 add_library(boost-iostreams
+	${LIBLZMA_CONFIG}
 	${boost-iostreams_SOURCES})
 
 target_link_libraries(boost-iostreams
-	${BZIP2_LIBRARIES})
-
-set(BOOST_IOSTREAMS_INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/system/include" CACHE PATH "boost system include path")
-mark_as_advanced(BOOST_IOSTREAMS_INCLUDE_DIRS)
-
+	${BZIP2_LIBRARIES}
+	${LIBLZMA_LIBRARIES})
